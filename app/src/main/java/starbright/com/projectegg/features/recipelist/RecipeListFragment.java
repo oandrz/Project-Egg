d7f13e37d3ed593a269fd23fffe6c025da4478f6
oandrz <oentoro.andreas@gmail.com>
2018-04-08 15:56:24 +0700

    implement hotchemi permission dispatcher

--- app/src/main/java/starbright/com/projectegg/features/recipelist/RecipeListFragment.java
+++ app/src/main/java/starbright/com/projectegg/features/recipelist/RecipeListFragment.java
@@ -1,14 +1,18 @@
 package starbright.com.projectegg.features.recipelist;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 import starbright.com.projectegg.R;
 import starbright.com.projectegg.util.ClarifaiHelper;
 
@@ -18,6 +22,7 @@ import static android.app.Activity.RESULT_OK;
  * Created by Andreas on 4/8/2018.
  */
 
+@RuntimePermissions
 public class RecipeListFragment extends Fragment implements ClarifaiHelper.Callback {
 
     private static final int CAMERA_REQUEST_CODE = 100;
@@ -44,7 +49,16 @@ public class RecipeListFragment extends Fragment implements ClarifaiHelper.Callb
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mClarifaiHelper = new ClarifaiHelper();
-        openCamera();
+        RecipeListFragmentPermissionsDispatcher.openCameraWithPermissionCheck(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        RecipeListFragmentPermissionsDispatcher
+                .onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     @Override
@@ -55,7 +69,8 @@ public class RecipeListFragment extends Fragment implements ClarifaiHelper.Callb
         }
     }
 
-    private void openCamera() {
+    @NeedsPermission(Manifest.permission.CAMERA)
+    public void openCamera() {
         startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQUEST_CODE);
     }
 
