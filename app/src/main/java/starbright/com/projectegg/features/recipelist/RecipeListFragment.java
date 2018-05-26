faa05d7b6c2d521f9484e7a4fc61f8b5a908f040
oandrz <oentoro.andreas@gmail.com>
2018-05-26 17:33:05 +0700

    test commit

--- app/src/main/java/starbright/com/projectegg/features/recipelist/RecipeListFragment.java
+++ app/src/main/java/starbright/com/projectegg/features/recipelist/RecipeListFragment.java
@@ -67,6 +67,7 @@ public class RecipeListFragment extends Fragment
     private RecipeListContract.Presenter mPresenter;
     private RecipeListAdapter mAdapter;
 
+
     String mCurrentPhotoPath;
 
     public static RecipeListFragment newInstance() {
