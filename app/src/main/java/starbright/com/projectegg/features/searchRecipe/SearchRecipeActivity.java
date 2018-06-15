469e03c93f28406aa14f08abec3e07e2f4342bfe
oandrz <oentoro.andreas@gmail.com>
2018-06-15 10:17:58 +0700

    add search recipe activity

---
+++ app/src/main/java/starbright/com/projectegg/features/searchRecipe/SearchRecipeActivity.java
@@ -0,0 +1,15 @@
+package starbright.com.projectegg.features.searchRecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import starbright.com.projectegg.R;
+
+public class SearchRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_recipe);
+    }
+}
