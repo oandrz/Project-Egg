6a7411adc6583abd5db68a8d57a5475fc4979a4c
oandrz <oentoro.andreas@gmail.com>
2018-08-15 11:14:39 +0700

    Add newIntent and newinstance method

--- app/src/main/java/starbright/com/projectegg/features/detail/RecipeDetailActivity.java
+++ app/src/main/java/starbright/com/projectegg/features/detail/RecipeDetailActivity.java
@@ -8,18 +8,53 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import starbright.com.projectegg.R;
 import starbright.com.projectegg.features.base.BaseActivityWithToolbar;
 
 public class RecipeDetailActivity extends BaseActivityWithToolbar {
 
-    public static Intent newIntent(Context context) {
-        return new Intent(context, RecipeDetailActivity.class);
+    private static final String RECIPE_FRAGMENT_TAG = "RECIPE_FRAGMENT_TAG";
+    private static final String EXTRA_RECIPE_ID = "EXTRA_RECIPE_ID";
+
+    private RecipeDetailFragment mFragment;
+
+    public static Intent newIntent(Context context, String recipeId) {
+        Intent intent = new Intent(context, RecipeDetailActivity.class);
+        intent.putExtra(EXTRA_RECIPE_ID, recipeId);
+        return intent;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getSupportFragmentManager().beginTransaction()
+                .attach(mFragment)
+                .commit();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initFragment();
     }
 
+    @Override
+    protected void onStop() {
+        getSupportFragmentManager().beginTransaction()
+                .detach(mFragment)
+                .commitAllowingStateLoss();
+        super.onStop();
+    }
 
+    private void initFragment() {
+        mFragment = (RecipeDetailFragment) getSupportFragmentManager()
+                .findFragmentByTag(RECIPE_FRAGMENT_TAG);
+        if (mFragment == null) {
+            mFragment = RecipeDetailFragment
+                    .newInstance(getIntent().getStringExtra(EXTRA_RECIPE_ID));
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFrame, mFragment, RECIPE_FRAGMENT_TAG)
+                    .commit();
+        }
+    }
 }
--- app/src/main/java/starbright/com/projectegg/features/detail/RecipeDetailFragment.java
+++ app/src/main/java/starbright/com/projectegg/features/detail/RecipeDetailFragment.java
@@ -12,10 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import starbright.com.projectegg.R;
+
 public class RecipeDetailFragment extends Fragment {
 
-    public static RecipeDetailFragment newInstance() {
+    private static final String BUNDLE_RECIPE_ID = "BUNDLE_RECIPE_ID";
+
+    public static RecipeDetailFragment newInstance(String recipeId) {
         Bundle args = new Bundle();
+        args.putString(BUNDLE_RECIPE_ID, recipeId);
         RecipeDetailFragment fragment = new RecipeDetailFragment();
         fragment.setArguments(args);
         return fragment;
@@ -23,7 +28,10 @@ public class RecipeDetailFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        return view;
     }
 }
