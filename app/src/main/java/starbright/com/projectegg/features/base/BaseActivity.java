c495637307ab12fec0161ae9405a982d419c817c
oandrz <oentoro.andreas@gmail.com>
2018-09-10 19:26:16 +0700

    Add admob lib

--- app/src/main/java/starbright/com/projectegg/features/base/BaseActivity.java
+++ app/src/main/java/starbright/com/projectegg/features/base/BaseActivity.java
@@ -1,8 +1,17 @@
+/**
+ * Created by Andreas on 10/9/2018.
+ */
+
 package starbright.com.projectegg.features.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.ads.MobileAds;
+
+import starbright.com.projectegg.BuildConfig;
 import starbright.com.projectegg.R;
 
 /**
@@ -10,6 +19,12 @@ import starbright.com.projectegg.R;
  */
 
 abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MobileAds.initialize(this, BuildConfig.ADMOB_APP_ID);
+    }
 
     @Override
     public void onBackPressed() {
