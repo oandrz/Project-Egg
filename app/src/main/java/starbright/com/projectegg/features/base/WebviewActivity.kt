f218079a47aec3c300930cba2f253ab4fbf5b70d
Andreas Oen <andreas.oentoro@bukalapak.com>
2019-11-02 17:09:08 +0700

    Update lib version

---
+++ app/src/main/java/starbright/com/projectegg/features/base/WebviewActivity.kt
@@ -0,0 +1,81 @@
+/**
+ * Created by Andreas on 27/8/2018.
+ */
+
+package starbright.com.projectegg.features.base
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+import junit.framework.Assert
+import kotlinx.android.synthetic.main.activity_webview.*
+
+import starbright.com.projectegg.R
+
+class WebviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        setupToolbar()
+        setupWebView()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar!!.setTitle(intent.getStringExtra(EXTRA_URL))
+        Assert.assertNotNull(actionBar)
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setHomeButtonEnabled(true)
+    }
+
+    private fun setupWebView() {
+        val webSettings = webview.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webview.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap) {
+                super.onPageStarted(view, url, favicon)
+                progress_bar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                progress_bar.visibility = View.GONE
+            }
+        }
+        webview.overScrollMode = WebView.OVER_SCROLL_NEVER
+        webview.loadUrl(intent.extras!!.getString(EXTRA_URL))
+    }
+
+    companion object {
+
+        val EXTRA_URL = "EXTRA_URL"
+
+        fun newIntent(context: Context, url: String): Intent {
+            val intent = Intent(context, WebviewActivity::class.java)
+            intent.putExtra(EXTRA_URL, url)
+            return intent
+        }
+    }
+}
